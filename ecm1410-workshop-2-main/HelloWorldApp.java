d377f8531ebff9924fd64981bf9b436d8138aadb
mj640 <mj640@exeter.ac.uk>
2026-02-09 16:04:58 +0000

    1) Coding HelloWorldApp.java

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,8 +1,9 @@
 // HelloWorld
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World! My name is " + args[0] + ".");
+        System.out.println("Hello World! My name is " + args[0] + ". I am " + args[1] + " years old.");
     }
 }
+
 
 
